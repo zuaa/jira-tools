08f8541cb0e95c448c1c5ef758db78e7d1601c5d
xuping <xuping08@sinochem.com>
2019-03-07 18:35:40 +0800

    修改敏感信息

--- src/main/java/cc/xwolf/url/Test2.java
+++ src/main/java/cc/xwolf/url/Test2.java
@@ -15,20 +15,14 @@ import net.rcarz.jiraclient.Project;
 public class Test2 {
     public static void main(String[] args) throws JiraException {
 
-        BasicCredentials creds = new BasicCredentials("xuping", "aaaaaaaa");
-        JiraClient jira = new JiraClient("http://jira.sinochemb2c.com", creds);
-        Project re =   jira.getProject("YXZB");
-        System.out.println(re); 
-       
-    
-    
-
-        ExcelReader reader = ExcelUtil.getReader("C:\\Users\\zuac\\Documents\\集团总控系统项目计划.xlsx");
+        BasicCredentials creds = new BasicCredentials("这里是用户名", "这里是密码");
+        JiraClient jira = new JiraClient("http://jira.域名.com", creds);
+        ExcelReader reader = ExcelUtil.getReader("C:\\Users\\zuac\\Documents\\文件名字.xlsx");
         List<List<Object>> readAll = reader.read();
-         for (int j = 1; j < readAll.size(); j++) {
+         for (int j = 1; j < readAll.size(); j++) {//跳过第一行
              System.out.println(readAll.get(j));
              List item = readAll.get(j);
-             Issue newIssue = jira.createIssue("YXZB", "Task")
+             Issue newIssue = jira.createIssue("YXZB", "Task")//项目的id 和 bug类型  
              .field(Field.SUMMARY, item.get(0))
              .field(Field.DESCRIPTION,  item.get(0))
              .field(Field.REPORTER, "xuping")
